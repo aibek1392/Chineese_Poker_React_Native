ab3f43d3acf1ffce0c99a4df1695adf7f5de8173
aibek <ozhorov@gmail.com>
2020-05-19 23:55:06 -0400

    tralala

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TextInput, AsyncStorage } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { styles } from '../styles.js';
 import firebase from 'firebase';
 import { firebaseConfig } from '../utils/firebaseConfig';
+import TextInput from '../components/TextInput';
 
 const Login = ({ navigation }) => {
 
@@ -10,14 +11,6 @@ const Login = ({ navigation }) => {
   const [password, setPassword] = useState('')
   const [userData, setUserData] = useState(null)
   const [signup, setSignup] = useState(false)
-
-  const handleEmail = (event) => {
-    setEmail(event.target.value)
-  }
-
-  const handlePassword = (event) => {
-    setPassword(event.target.value)
-  }
 
   const firebaseInit = () => {
     if (firebase.apps.length === 0) {
@@ -31,9 +24,11 @@ const Login = ({ navigation }) => {
       .then(resp => {
         if (resp){
           setUserData(true)
-          AsyncStorage.setItem(`userData`, JSON.stringify(resp))
         }
-    })
+     })
+    .catch(err => {
+      console.log('Error: ', err);
+    });
   }
 
   const handleSignup = () => {
@@ -42,9 +37,14 @@ const Login = ({ navigation }) => {
       .then(resp => {
         if (resp){
           setUserData(true)
-          AsyncStorage.setItem(`userData`, JSON.stringify(resp))
+          firebase.firestore().collection('users').doc(resp.user.uid).set({
+            email: email,
+          })
         }
-    })
+      })
+      .catch(err => {
+        console.log('Error: ', err);
+      });
   }
 
   const authToggle = () => {
@@ -58,12 +58,12 @@ const Login = ({ navigation }) => {
 return (
       <View style={styles.center}>
         <Text style={styles.title}>Email</Text>
-        <TextInput style={styles.input} autoCapitalize='none' onChange={handleEmail}/>
+        <TextInput placeholder="Email" value={email} onChangeText={text => setEmail(text)} />
         <Text style={styles.title}>Password</Text>
-        <TextInput style={styles.input} secureTextEntry autoCapitalize='none' onChange={handlePassword}/>
+        <TextInput placeholder="Password" value={password} onChangeText={text => setPassword(text)} textInputProps={{ secureTextEntry: true }} />
         <Button title={signup ? `Already have an account?` : `New user?`} onPress={authToggle} />
         <Button title={signup ? 'Signup' : 'Login'} onPress={signup ? handleSignup : handleLogin}/>
-        {userData ? auth() : null}
+        {userData && auth()}
       </View>
     );
 }
