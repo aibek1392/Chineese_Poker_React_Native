7bdd31f515975afbee2581271e83a9363874efed
aibek <ozhorov@gmail.com>
2020-06-05 17:30:12 -0400

    changes on lobby

--- src/screens/Lobby.js
+++ src/screens/Lobby.js
@@ -1,60 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RedButton from '../components/Button'
 import {
   View,
   StyleSheet,
-  Text,
-  Alert,
-  Button,
 } from "react-native";
-import Modal from "react-native-modal";
 import UserCard from '../components/UserCard';
+import firebase from '../utils/firebaseConfig';
+import '@firebase/firestore';
 
 const Lobby = ({ navigation }) => {
-  const [isModalVisible, setModal] = useState(false);
-  const [userName, setUserName] = useState("");
+  const [user, setUser] = useState({})
 
-  const user = {
-    username: 'Aibek',
-    avatar: 'https://i1.pngguru.com/preview/137/834/449/cartoon-cartoon-character-avatar-drawing-film-ecommerce-facial-expression-png-clipart.jpg' 
-  }
+  useEffect(() => {
+    setCurrentUser()
+  })
 
-  const handleNo = () => {
-    Alert.alert("Sorry", "You must be 21 or older to play!", [
-      {
-        text: "Ok",
-        onPress: () => {
-          setModal(false)
-          navigation.navigate("Home")
-        }
-      },
-    ]);
-  };
-
-  const handleYes = () => {
-    setModal(false)
-    navigation.navigate('Play', { userName: userName })
+  const setCurrentUser = () => {
+    let currentUser = firebase.auth().currentUser
+    
+    firebase.firestore().collection('users').doc(currentUser.uid).get()
+    .then(resp => {
+      if (!resp.exists) {
+        console.log('No such User!');
+      } else {
+        setUser(resp.data())
+      }
+    })
+    .catch(err => {
+      console.log('Error: ', err);
+    });
   }
 
   return (
     <View style={styles.container}>
-      <UserCard username={user.username} avatar={user.avatar}/>
+      <UserCard username={user.email} />
       <UserCard/>
       <UserCard/>
       <UserCard/>
-
-      <RedButton width='30%' title="Next" onPress={() => setModal(true)} />
-
-      <Modal isVisible={isModalVisible} style={styles.modalContainer}>
-        <View style={styles.modalView}>
-          <Text>Are you over 21?</Text>
-          <View style={{ flexDirection: "row" }}>
-            <Button title="Yes" onPress={handleYes} />
-            <Button title="No" onPress={handleNo} />
-          </View>
-        </View>
-      </Modal>
-
+      <RedButton width='30%' title="Next" onPress={() => navigation.navigate('Play')} />
     </View>
   );
 };
