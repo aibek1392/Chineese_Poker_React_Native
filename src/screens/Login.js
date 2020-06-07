75c240a107543fd1b21cfa7fb73fac83f69dec43
aibek <ozhorov@gmail.com>
2020-06-07 19:40:05 -0400

    cleanup loginPage

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,88 +1,56 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
-// import { styles } from '../styles.js';
-import firebase from 'firebase';
-import { firebaseConfig } from '../utils/firebaseConfig';
-import TextInput from '../components/TextInput';
+import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity, TextInput } from 'react-native';
+import firebase from '../utils/firebaseConfig';
+import TI from '../components/TextInput';
 import Button from '../components/Button';
 import { colors } from '../utils/Theme';
+import { useFonts } from '@use-expo/font'
 import { BorderlessButton } from 'react-native-gesture-handler';
 
 const width = Dimensions.get('window').width
 
 const Login = ({ navigation }) => {
+  
+  let [fonts] = useFonts({
+    'Chelsea-Market': require('../../assets/fonts/Chelsea-Market.ttf'),
+    'Dosis': require('../../assets/fonts/Dosis-Regular.ttf'),
+  })
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [userData, setUserData] = useState(null)
-  const [signup, setSignup] = useState(false)
-
-  const firebaseInit = () => {
-    if (firebase.apps.length === 0) {
-    return  firebase.initializeApp(firebaseConfig);
-   }
-  }
 
   const handleLogin = () => {
-    firebaseInit()
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(resp => {
-        if (resp){
-          setUserData(true)
-        }
-     })
     .catch(err => {
       console.log('Error: ', err);
     });
   }
 
-  const handleSignup = () => {
-    firebaseInit()
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(resp => {
-        if (resp){
-          setUserData(true)
-          firebase.firestore().collection('users').doc(resp.user.uid).set({
-            email: email,
-          })
-        }
-      })
-      .catch(err => {
-        console.log('Error: ', err);
-      });
-  }
-
-  const authToggle = () => {
-    setSignup(!signup)
-  }
-
-  const auth = () => {
-    navigation.navigate('Home')
-  }
-
-return (
+  if (!fonts) {
+    return null
+  } else {
+    return (
       <View style={styles.container}>
         <View style={styles.titleContainer}>
           <Image 
             source={require('../../assets/cards.jpg')} 
             style={styles.img}
           />
-          <Text style={{color: colors.yellow, fontSize: 36, fontWeight: 'bold'}}>Big 2</Text>
+          <Text style={styles.title}>BIG 2</Text>
         </View>
         <View style={styles.login}>
-          {/* <Text style={styles.title}>Email</Text> */}
-          <TextInput placeholder="Email" value={email} onChangeText={text => setEmail(text)} />
-          {/* <Text style={styles.title}>Password</Text> */}
-          <TextInput placeholder="Password" value={password} onChangeText={text => setPassword(text)} textInputProps={{ secureTextEntry: true }} />
-          <Button title={signup ? 'SIGN UP' : 'LOG IN'} onPress={signup ? handleSignup : handleLogin} width={width-50} />
-          <Text>Don't have an account? 
-              <Text style={{ fontWeight: 'bold'}}> Sign Up</Text> 
-          </Text>
-          {/* <Button title={signup ? `Already have an account?` : `New user?`} onPress={authToggle} width={width-50} /> */}
-          {userData && auth()}
+          <TI placeholder="Email" value={email} onChangeText={text => setEmail(text)} />
+          <TI placeholder="Password" value={password} onChangeText={text => setPassword(text)} textInputProps={{ secureTextEntry: true }} />
+          <Button title='LOG IN' onPress={handleLogin} width={width-50} />
+          <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+            <Text style={{ fontFamily: 'Dosis' }}>Don't have an account?
+              <Text style={{ fontWeight: 'bold' }}> Sign Up</Text> 
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -95,6 +63,11 @@ const styles = StyleSheet.create({
   titleContainer: {
     alignItems: 'center',
     margin: 20
+  },
+  title: {
+    color: colors.yellow,
+    fontSize: 42,
+    fontFamily: 'Chelsea-Market'
   },
   img: {
     width: 100,
